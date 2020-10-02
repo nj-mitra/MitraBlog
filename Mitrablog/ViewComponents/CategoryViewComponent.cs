a6df6d23ae56a52f3c7f5b33c13ccad79d44d149
MitraNajafi <njmitra8@gmail.com>
2020-10-02 13:45:10 +0330

    add category list in dropdown menu

---
+++ Mitrablog/ViewComponents/CategoryViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mitrablog.Models;
+using Mitrablog.Models.Posts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mitrablog.ViewComponents
+{
+    public class CategoryViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            using (var ctx = new ApplicationContext())
+            {
+                List<PostCategory> postCategories = ctx.PostCategories.ToList();
+                return View(postCategories);
+            }
+        }
+    }
+}
